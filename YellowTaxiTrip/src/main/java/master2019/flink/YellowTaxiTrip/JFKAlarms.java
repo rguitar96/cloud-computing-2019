5914f937f8b41db63284f6f1d5089df7040ed62b
Rodrigo <rodrigo.pueblas@hotmail.com>
2020-01-08 21:33:02 +0100

    Executes, but not correct output for JFK

--- YellowTaxiTrip/src/main/java/master2019/flink/YellowTaxiTrip/JFKAlarms.java
+++ YellowTaxiTrip/src/main/java/master2019/flink/YellowTaxiTrip/JFKAlarms.java
@@ -3,13 +3,17 @@ package master2019.flink.YellowTaxiTrip;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.tuple.Tuple4;
-import org.apache.flink.api.java.tuple.Tuple18;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
+import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 
@@ -17,6 +21,7 @@ import java.time.format.DateTimeFormatter;
  * In this class the JFK airport trips program has to be implemented.
  */
 public class JFKAlarms {
+
     public static void main(String[] args) throws Exception {
         System.out.println("Starting JFK Execution...");
 
@@ -24,7 +29,9 @@ public class JFKAlarms {
 
         // set up the execution environment
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-/*
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        /*
         String inputPath = "";
         String outputPath = "";
         try {
@@ -35,8 +42,8 @@ public class JFKAlarms {
             return;
         }
  */
-        String inputPath = "/home/rodrigo/flink/assignment/YellowTaxiTrip/yellow_tripdata_2019_06.csv";
-        String outputPath = "/home/rodrigo/flink/assignment/YellowTaxiTrip/output/";
+        String inputPath = "/home/rodrigo/flink/cloud-computing-2019/YellowTaxiTrip/YellowTaxiTrip/yellow_tripdata_2019_06.csv";
+        String outputPath = "/home/rodrigo/flink/cloud-computing-2019/YellowTaxiTrip/YellowTaxiTrip/output/";
         String	outFilePathJFK = outputPath +  "/jfk.csv";
         DataStream<String> source = env.readTextFile(inputPath).setParallelism(1);
 
@@ -47,7 +54,8 @@ public class JFKAlarms {
             public boolean filter(String value) {
                 String[] s = value.split(",");
                 int passengerCount = Integer.parseInt(s[3]);
-                return passengerCount >= 2;
+                boolean is_jfk = Integer.parseInt(s[5]) == 2;
+                return passengerCount >= 2 && is_jfk;
             }
         }).map(new MapFunction<String, Tuple4<Integer, LocalDateTime, LocalDateTime, Integer>>() {
             @Override
@@ -57,7 +65,13 @@ public class JFKAlarms {
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
                 return new Tuple4<Integer, LocalDateTime, LocalDateTime, Integer>(Integer.parseInt(s[0]), LocalDateTime.parse(s[1], formatter), LocalDateTime.parse(s[1], formatter), Integer.parseInt(s[3]));
             }
-        });
+        }).assignTimestampsAndWatermarks(new AscendingTimestampExtractor<Tuple4<Integer, LocalDateTime, LocalDateTime, Integer>>() {
+            @Override
+            public long extractAscendingTimestamp(Tuple4<Integer, LocalDateTime, LocalDateTime, Integer> input) {
+                return input.f1.atZone(ZoneId.of("America/New_York")).toInstant().toEpochMilli();
+            }
+        }).keyBy(1).timeWindow(Time.hours(1)).sum(3);
+
 
         // emit result
         taxiTrips.writeAsText(outFilePathJFK);
